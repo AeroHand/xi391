50334a3dddc41fa5eabb63e8a9083c786763725e
Robert Stein <rstein92@gmail.com>
2012-10-21 14:17:29 -0500

    wrote a good draft of rtc_init

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -8,26 +8,31 @@
 void
 rtc_init(void) {
 
-	/* This is how we will -learn- do this.
-	 * ::
-	 * Do this:
-	 * so the first thing you need to do is set bit 7 of reg b to 1 to prevent
-	 * updates from occuring while init-ing
-	 * then init time, cal, etc
-	 * [ figure out how to change clock freq ]
-	 *    change freq:
-	 * 	   set rs 3 through 0 
-	 * then set reg b bit 7 to 0
-	 * READ PAGE 18, 19, 15?
+	/*
+	 * set B[7] (SET) to 1 - make update transfter inhibited?
+	 * set A[6:4] (DV) to 010 - turn on oscillator/allow RTC to keep time
+	 * set A[3:0] (RS) to 0110 - set interrupt rate to 1.024 kHz
+	 * set B[6] (PIE) to 1 - to turn on period interrupts
+	 * set B[5] (AIE) to 0 - not allow alarm flag to assert irq'
+	 * set B[3] (SQWE) to 1 - enable square wave
+	 * set B[2] (DM) to 1 - calendar information stored in binary
+	 * set B[1] to 1 - enable 24 hour mode
 	 */
 
-	/* FIXME */
-	/* select Status Register A, and disable NMI (by setting the 0x80 bit) */
-	outb(0x8A, 0x70); 
-	/* write to CMOS/RTC RAM */
-	outb(0x20, 0x71); 
+	/* Mask all interrupts? */
+
+	/* select rtc register B and disable NMI by setting the 7th bit */
+	outb(0x8B, 0x70);
+	/* Set PIE=1 and stuff */
+	outb(0xDE, 0x71);
+	/* select rtc register A and disable NMI by setting the 7th bit */
+	outb(0x8A, 0x70);
+	/* Set RS[3:0]=0110 and stuff */
+	outb(0x26, 0x71);
+
+	/* Here Initialze the time and calendar and stuff? */
+	/* Here set B[7] (SET) back to 0? */
 
 	/* Unmask RTC */
 	enable_irq(8);
-
 }
